679ae89ed6f428c7d045d092013364eee51682a8
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-18 18:06:23 +0700

    update

---
+++ app/api/frame-3/route.ts
@@ -0,0 +1,61 @@
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
+import { NEXT_PUBLIC_URL } from '../../config';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+  if (!isValid) {
+    return new NextResponse('Message not valid', { status: 500 });
+  }
+
+  const text = message.input || '';
+  let state = {
+    page: 0,
+  };
+  try {
+    state = JSON.parse(decodeURIComponent(message.state?.serialized));
+  } catch (e) {
+    console.error(e);
+  }
+
+  /**
+   * Use this code to redirect to a different page
+   */
+  if (message?.button === 3) {
+    return NextResponse.redirect(
+      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
+      { status: 302 },
+    );
+  }
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          action: 'link',
+          label: 'Nominate Now',
+          target: `https://build.top/nominate/0xAa66fC4433DFD254F05A720A8c8a2f1F2fbDCB58`,
+        },
+        
+        {
+          label: `Next`,
+          target: `${NEXT_PUBLIC_URL}/api/frame-4`,
+         
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/build.png`,
+      },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+      
+    }),
+  );
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
